7cccc97bd775f37a06c491b60b4c93620ab6a48b
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2023-09-15 16:58:01 +0200

    feat: added CNNAutoEncoder class which is composed of add CNNEncoder and CNNDecoder class

--- src/random_neural_net_models/cnn_autoencoder_fastai2022.py
+++ src/random_neural_net_models/cnn_autoencoder_fastai2022.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import torch
 import torch.nn as nn
 from einops.layers.torch import Rearrange
 
@@ -29,6 +30,75 @@ class DeConv2d(nn.Module):
         return self.net(x)
 
 
+class CNNEncoder(nn.Module):
+    # based on https://avandekleut.github.io/vae/
+    def __init__(self, ks: int = 3):
+        super(CNNEncoder, self).__init__()
+
+        stride = 2
+        padding = ks // 2
+        self.add_dim = Rearrange("b h w -> b 1 h w")
+        self.add_padding = nn.ZeroPad2d(2)
+        self.enc_conv1 = nn.Conv2d(
+            1, 2, kernel_size=ks, stride=stride, padding=padding
+        )
+        self.enc_act1 = nn.ReLU()
+        self.enc_conv2 = nn.Conv2d(
+            2, 4, kernel_size=ks, stride=stride, padding=padding
+        )
+        self.enc_act2 = nn.ReLU()
+
+        self.encoder = nn.Sequential(
+            self.add_dim,  # 28x28 -> 1x28x28
+            self.add_padding,  # 1x28x28 -> 1x32x32
+            self.enc_conv1,  # 1x32x32 -> 1x16x16
+            self.enc_act1,
+            self.enc_conv2,  # 1x16x16 -> 1x8x8
+            self.enc_act2,
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        z = self.encoder(x)
+        return z
+
+
+class CNNDecoder(nn.Module):
+    def __init__(self, ks: int = 3):
+        super(CNNDecoder, self).__init__()
+        self.dec_deconv1 = DeConv2d(4, 2, kernel_size=ks, stride=1)
+        self.dec_act1 = nn.ReLU()
+        self.dec_deconv2 = DeConv2d(2, 1, kernel_size=ks, stride=1)
+        self.dec_act2 = nn.Sigmoid()
+        self.rm_padding = nn.ZeroPad2d(-2)
+        self.rm_dim = Rearrange("b 1 h w -> b h w")
+
+        self.decoder = nn.Sequential(
+            self.dec_deconv1,  # 1x8x8 -> 1x16x16
+            self.dec_act1,
+            self.dec_deconv2,  # 1x16x16 -> 1x32x32
+            self.rm_padding,  # 1x32x32 -> 1x28x28
+            self.dec_act2,
+            self.rm_dim,  # 1x28x28 -> 28x28
+        )
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        x_hat = self.decoder(z)
+        return x_hat
+
+
+class CNNAutoEncoder(nn.Module):
+    def __init__(self, ks: int = 3) -> None:
+        super(CNNAutoEncoder, self).__init__()
+        self.encoder = CNNEncoder(ks=ks)
+        self.decoder = CNNDecoder(ks=ks)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+
+
+# TODO: remove once refactoring completed
 class Model(nn.Module):
     # https://github.com/fastai/course22p2/blob/master/nbs/08_autoencoder.ipynb
     def __init__(self):
