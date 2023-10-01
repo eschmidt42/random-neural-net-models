4d344d5c0e18204cac29d192ea88500bba71709b
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2023-10-01 08:54:46 +0200

    feat: added make_deterministic function

--- src/random_neural_net_models/utils.py
+++ src/random_neural_net_models/utils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
+import random
 
+import numpy as np
+import torch
 from rich.logging import RichHandler
 
 
@@ -20,3 +23,18 @@ def get_logger(name: str = "rich", level=logging.INFO):
 
 
 logger = get_logger("utils.py")
+
+
+def make_deterministic(seed: int = 42):
+    # from https://krokotsch.eu/posts/deep-learning-unit-tests/
+    # PyTorch
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    # Numpy
+    np.random.seed(seed)
+
+    # Built-in Python
+    random.seed(seed)
