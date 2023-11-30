ee0529e927b07cb61faf3cce2c839aaacf9776be
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2023-11-30 19:25:08 +0100

    chore: made random seeming constants in NewGELU to module parameters

--- src/random_neural_net_models/mingpt/model.py
+++ src/random_neural_net_models/mingpt/model.py
@@ -30,9 +30,22 @@ class NewGELU(nn.Module):
 
     C = math.sqrt(2.0 / math.pi)
 
+    def __init__(
+        self,
+        f0: float = 0.044715,
+        f1: float = 3.0,
+        f2: float = 0.5,
+        f3: float = 1.0,
+    ):
+        super().__init__()
+        self.f0 = f0
+        self.f1 = f1
+        self.f2 = f2
+        self.f3 = f3
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        a = x + 0.044715 * torch.pow(x, 3.0)
-        return 0.5 * x * (1 + torch.tanh(self.C * a))
+        a = x + self.f0 * torch.pow(x, self.f1)
+        return self.f2 * x * (self.f3 + torch.tanh(self.C * a))
 
 
 class CausalSelfAttention(nn.Module):
@@ -45,13 +58,17 @@ class CausalSelfAttention(nn.Module):
     def __init__(self, config: configs.ModelConfig):
         super().__init__()
         assert config.n_embd % config.n_head == 0
+
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
         # regularization
         self.attn_dropout = nn.Dropout(config.attn_pdrop)
         self.resid_dropout = nn.Dropout(config.resid_pdrop)
+
         # causal mask to ensure that attention is only applied to the left in the input sequence
         self.register_buffer(
             "bias",
