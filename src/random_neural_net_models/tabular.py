25bece7dca4b0efab89c75d7f53b1e3ebc41fccb
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2024-03-11 19:48:46 +0100

    feat: added ImputeMissingness pytorch module, integrated it into TabularModel and added tests

--- src/random_neural_net_models/tabular.py
+++ src/random_neural_net_models/tabular.py
@@ -4,6 +4,8 @@ import typing as T
 import torch
 import random_neural_net_models.data as rnnm_data
 import random_neural_net_models.utils as utils
+from enum import Enum
+import numpy as np
 
 logger = utils.get_logger("tabular.py")
 
@@ -32,13 +34,61 @@ class Layer(nn.Module):
         return self.net(X)
 
 
+class BiasSources(Enum):
+    zero = "zero"
+    normal = "normal"
+
+
+class ImputeMissingness(nn.Module):
+    def __init__(self, n_features: int, bias_source: BiasSources):
+        super().__init__()
+
+        match bias_source:
+            case BiasSources.zero:
+                bias = torch.zeros((1, n_features), dtype=torch.float)
+            case BiasSources.normal:
+                bias = torch.rand((1, n_features), dtype=torch.float)
+            case _:
+                raise NotImplementedError(
+                    f"{bias_source=} is not implemented in BiasSources, knows members: {BiasSources._member_names_()}"
+                )
+
+        self.bias = nn.Parameter(bias)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        is_finite = torch.isfinite(X)
+        is_finite = is_finite  # .to(X.device)
+        is_infinite = torch.logical_not(is_finite)
+        bias = self.bias.expand(X.shape[0], -1)  # .to(X.device)
+        X_imputed = X.masked_fill(is_infinite, 0)
+        X_imputed += bias.masked_fill(is_finite, 0)
+
+        is_infinite = is_infinite.float()
+        X_out = torch.hstack((X_imputed, is_infinite))
+        return X_out
+
+
 class TabularModel(nn.Module):
     def __init__(
-        self, n_hidden: T.List[int], use_batch_norm: bool
-    ) -> None:  # ,task:Task
+        self,
+        n_hidden: T.List[int],
+        use_batch_norm: bool,
+        do_impute: bool = False,
+        impute_bias_source: BiasSources = BiasSources.zero,
+    ) -> None:
         super().__init__()
 
         layers = []
+        if do_impute:
+            layers.append(
+                ImputeMissingness(
+                    n_features=n_hidden[0], bias_source=impute_bias_source
+                )
+            )
+            n_hidden[0] = (
+                n_hidden[0] * 2
+            )  # because ImputeMissingness horizontally stacks boolean missingness flags
+
         for i, (n_in, n_out) in enumerate(zip(n_hidden[:-1], n_hidden[1:])):
             is_not_last = i <= len(n_hidden) - 3
 
@@ -64,11 +114,18 @@ class TabularModelClassification(nn.Module):
         n_hidden: T.List[int],
         n_classes: int,
         use_batch_norm: bool,
-    ) -> None:  # ,task:Task
+        do_impute: bool = False,
+        impute_bias_source: BiasSources = BiasSources.zero,
+    ) -> None:
         super().__init__()
 
         n_acts = [n_features] + n_hidden + [n_classes]
-        self.net = TabularModel(n_hidden=n_acts, use_batch_norm=use_batch_norm)
+        self.net = TabularModel(
+            n_hidden=n_acts,
+            use_batch_norm=use_batch_norm,
+            do_impute=do_impute,
+            impute_bias_source=impute_bias_source,
+        )
 
     def forward(self, input: rnnm_data.XyBlock) -> torch.Tensor:
         return self.net(input)
@@ -93,16 +150,34 @@ class TabularModelRegression(nn.Module):
         mean: float,
         std: float,
         use_batch_norm: bool,
+        do_impute: bool = False,
+        impute_bias_source: BiasSources = BiasSources.zero,
     ) -> None:
         super().__init__()
 
         n_acts = (
             [n_features] + n_hidden + [1]
         )  # hard-coded that only one target is predicted
-        self.net = TabularModel(n_hidden=n_acts, use_batch_norm=use_batch_norm)
+        self.net = TabularModel(
+            n_hidden=n_acts,
+            use_batch_norm=use_batch_norm,
+            do_impute=do_impute,
+            impute_bias_source=impute_bias_source,
+        )
         self.scaler = StandardNormalScaler(mean=mean, std=std)
 
     def forward(self, input: rnnm_data.XyBlock) -> torch.Tensor:
         y = self.net(input)
         y = self.scaler(y)
         return y
+
+
+def make_missing(
+    X: np.ndarray, p_missing: float = 0.1
+) -> T.Tuple[np.ndarray, np.ndarray]:
+    mask = np.random.choice(
+        [False, True], size=X.shape, p=[1 - p_missing, p_missing]
+    )
+    X_miss = X.copy()
+    X_miss[mask] = float("inf")
+    return X_miss, mask
--- src/tests/test_tabular.py
+++ src/tests/test_tabular.py
@@ -4,6 +4,7 @@ import pytest
 import random_neural_net_models.tabular as rnnm_tab
 import random_neural_net_models.data as rnnm_data
 import random_neural_net_models.losses as rnnm_loss
+import numpy as np
 
 
 @pytest.mark.parametrize("use_batch_norm", [True, False])
@@ -49,7 +50,7 @@ def test_tabular_model(
 @pytest.mark.parametrize("n_features", [1, 4])
 def test_highlevel_tabular_model_for_classification(
     use_batch_norm: bool, n_classes: int, n_features: int
-):  # use_batch_norm: bool
+):
     model = rnnm_tab.TabularModelClassification(
         n_features=n_features,
         n_hidden=[5],
@@ -95,6 +96,117 @@ def test_highlevel_tabular_model_for_regression(
     )
     bs = 32
     x = torch.randn(bs, n_features)
+    y = torch.randn(size=(bs,))
+    input = rnnm_data.XyBlock(x=x, y=y, batch_size=[bs])
+    inference = model(input)
+    assert inference.shape == (bs, 1)
+    assert torch.isfinite(inference).all()
+
+    loss_fn = rnnm_loss.MSELossXy()
+    loss = loss_fn(inference, input)
+
+    assert torch.isfinite(loss)
+
+
+def test_impute_missingness():
+    n_features = 3
+    imputer = rnnm_tab.ImputeMissingness(
+        n_features, bias_source=rnnm_tab.BiasSources.zero
+    )
+
+    X = torch.tensor(
+        [[1, 2, float("inf")], [3, float("inf"), 6], [float("inf"), 5, 6]],
+        dtype=torch.float32,
+    )
+    expected_output = torch.tensor(
+        [[1, 2, 0, 0, 0, 1], [3, 0, 6, 0, 1, 0], [0, 5, 6, 1, 0, 0]],
+        dtype=torch.float32,
+    )
+
+    output = imputer(X)
+
+    assert torch.allclose(output, expected_output)
+    assert output.shape == (3, 6)
+    assert torch.isfinite(output[:, :n_features]).all()
+    assert torch.isfinite(output).all()
+
+
+def test_make_missing():
+    X = np.random.randn(1_000, 10)
+    p_missing = 0.1
+
+    X_miss, mask = rnnm_tab.make_missing(X, p_missing)
+
+    assert X_miss.shape == X.shape
+    assert mask.shape == X.shape
+
+    # Check if missing values are correctly set to infinity
+    assert np.all(X_miss[mask] == float("inf"))
+
+    # Check if non-missing values are unchanged
+    assert np.all(X_miss[~mask] == X[~mask])
+
+    # Check if the proportion of missing values is approximately equal to p_missing
+    assert np.isclose(np.sum(mask) / X.size, p_missing, atol=0.01)
+
+
+@pytest.mark.parametrize(
+    "bias_source", [rnnm_tab.BiasSources.zero, rnnm_tab.BiasSources.normal]
+)
+def test_highlevel_tabular_model_for_classification_with_missingness(
+    bias_source: rnnm_tab.BiasSources,
+):
+    n_classes = 2
+    n_features = 5
+
+    model = rnnm_tab.TabularModelClassification(
+        n_features=n_features,
+        n_hidden=[5],
+        n_classes=n_classes,
+        use_batch_norm=False,
+        do_impute=True,
+        impute_bias_source=bias_source,
+    )
+    bs = 32
+    x = np.random.randn(bs, n_features)
+    x, _ = rnnm_tab.make_missing(x, p_missing=0.5)
+    x = torch.from_numpy(x).float()
+    y = torch.randint(low=0, high=n_classes, size=(bs,))
+    input = rnnm_data.XyBlock(x=x, y=y, batch_size=[bs])
+    inference = model(input)
+    assert inference.shape == (bs, n_classes)
+    assert torch.isfinite(inference).all()
+
+    loss_fn = rnnm_loss.CrossEntropyXy()
+    loss = loss_fn(inference, input)
+
+    assert torch.isfinite(loss)
+
+
+@pytest.mark.parametrize(
+    "bias_source", [rnnm_tab.BiasSources.zero, rnnm_tab.BiasSources.normal]
+)
+def test_highlevel_tabular_model_for_regression_with_missingness(
+    bias_source: rnnm_tab.BiasSources,
+):
+    n_features = 5
+    mean = 0.0
+    std = 1.0
+
+    model = rnnm_tab.TabularModelRegression(
+        n_features=n_features,
+        n_hidden=[5],
+        mean=mean,
+        std=std,
+        use_batch_norm=False,
+        do_impute=True,
+        impute_bias_source=bias_source,
+    )
+
+    bs = 32
+    x = np.random.randn(bs, n_features)
+    x, _ = rnnm_tab.make_missing(x, p_missing=0.5)
+    x = torch.from_numpy(x).float()
     y = torch.randn(size=(bs,))
     input = rnnm_data.XyBlock(x=x, y=y, batch_size=[bs])
     inference = model(input)
